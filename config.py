1f5712720925e1f76834a53113a46dfcb09223ed
ashwinjo <ashwin.joshi@keysight.com>
2023-05-31 09:58:10 -0700

    Refactored code

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
 TARGET_CHASSIS_DICT = {
         "10.36.237.131": {
             "username": "admin",
-            "password": "somepassword"
+            "password": "Ixiaixiaixia!23"
         }
     }
+
+
+CONTROLLER_CHASSIS_DICT = {"ip": "10.36.236.121", "username": "admin", "password": "Kimchi123Kimchi123!"}
--- controller.py
+++ controller.py
@@ -1,34 +1,36 @@
+import logging
+import time
+
 from RestApi.IxOSRestInterface import IxRestSession
-import ixUpgradeUtility
-from config import TARGET_CHASSIS_DICT
+from config import TARGET_CHASSIS_DICT, CONTROLLER_CHASSIS_DICT
 
 def get_controller_chassis_packages(ip=None, username=None, password=None):
     """This method will return list of available updates"""
-    new_updates = []
+    new_packages_dict = []
     session = IxRestSession(ip, username, password, verbose=False)
-    out = ixUpgradeUtility.get_packages_on_controller_chassis(session)
+    out = session.get_packages()
     for updates in out:
         updates.pop('links')
-        new_updates.append(updates)
-    return new_updates
+        new_packages_dict.append(updates)
+    return new_packages_dict
     
 
 def add_target_ixia_chassis_to_controller_chassis(ip=None, username=None, password=None, target_chassis_list=None):
     session = IxRestSession(ip, username, password, verbose=False)
-    result_url =  ixUpgradeUtility.add_target_chassis_to_controller_chassis(session, target_chassis_list)
+    result_url = session.add_target_chassis_to_controller_chassis(target_chassis_list)
     return result_url
      
      
 def authenticate_target_chassis(ip=None, username=None, password=None, list_target_chassis_only_dict=None):
     session = IxRestSession(ip, username, password, verbose=False)
-    ixUpgradeUtility.authenticate_target_chassis(session, list_target_chassis_only_dict)
+    return session.authenticate_target_chassis(list_target_chassis_only_dict)
     
     
 def get_target_chassis_information(ip=None, username=None, password=None, target_chassis_list=None):
     session = IxRestSession(ip, username, password, verbose=False)
     target_chassis_only_dict = {}
     list_target_chassis_only_dict = []
-    existing_target_chassis =  ixUpgradeUtility.get_target_chassis_information(session)
+    existing_target_chassis =  session.get_target_chassis_information()
     for item in existing_target_chassis['data']:
         if not item['local']:
             target_chassis_only_dict['id'] = item['id']
@@ -42,32 +44,83 @@ def get_target_chassis_information(ip=None, username=None, password=None, target
 
 def upgrade_target_chassis(ip=None, username=None, password=None,uploadId=None, list_target_chassis_only_dict=None):
     session = IxRestSession(ip, username, password, verbose=False)
-    ixUpgradeUtility.upgrade_target_chassis(session, uploadId=uploadId, list_target_chassis_only_dict=list_target_chassis_only_dict)
-
-# Please select what version you want to upgrade to.
-packages = get_controller_chassis_packages(ip="10.36.236.121", username="admin", password="somepassword")
-print(packages)
+    return session.upgrade_target_chassis(uploadId=uploadId, list_target_chassis_only_dict=list_target_chassis_only_dict)
 
 
-# Add chassis as per configuration file
-add_target_ixia_chassis_to_controller_chassis(ip="10.36.236.121", username="admin", password="somepassword", 
-                                            target_chassis_list=list(TARGET_CHASSIS_DICT.keys()))
+
+def upgrade_commander():
+    ip = CONTROLLER_CHASSIS_DICT["ip"]
+    username = CONTROLLER_CHASSIS_DICT["username"]
+    password = CONTROLLER_CHASSIS_DICT["password"]
+    
+    logging.basicConfig(level=logging.INFO)
+    logging.info(f"Controller Chassis is {ip} with username : {username} and password : {password}")
+
+    logging.info(f"===== Fetching exisiting upgrade packages present on Controller Chassis {ip}")
+    # Please select what version you want to upgrade to.
+    packages = get_controller_chassis_packages(ip=ip, username=username, password=password)
+    print(f"ID - VERSION")
+    for p in packages:
+        print(f"{p['id']} - {p['version']}")
+    uploadId = input('Please enter ID of version you want to upgeade to: \n')
+    confirmation = input(f"You have selected to update target chassis to ID: {uploadId}. Please enter [Y/y] to proceed or [N/n] to re-enter: ")
+    if confirmation.lower() == "y":
+        logging.info(f"===== Adding Target Chassis from config file on Controller Chassis {ip}")
+        add_target_ixia_chassis_to_controller_chassis(ip=ip, username=username, password=password, target_chassis_list=list(TARGET_CHASSIS_DICT.keys()))
+        logging.info(f"===== Target Chassis added on Controller Chassis {ip}. Please check at https://{ip}/chassislab/chassisui/settings/ixos-install")
+        
+        logging.info(f"===== Consolidating chassis credentials and target chassis id's added in previous step")
+        list_target_chassis_only_dict = get_target_chassis_information(ip=ip, username=username, password=password, target_chassis_list=TARGET_CHASSIS_DICT)
+        logging.info(list_target_chassis_only_dict)
+        
+        logging.info(f"===== Authenticating all target chassis added on Controller Chassis {ip}")
+        authenticate_target_chassis(ip=ip, username=username, password=password, list_target_chassis_only_dict=list_target_chassis_only_dict)
+        logging.info(f"===== Authentication complete for target chassis. Please check at https://{ip}/chassislab/chassisui/settings/ixos-install")
+        
+        logging.info(f"===== Sleeping for 30 secs to ensure all target chassis complete authentication =====")
+        time.sleep(10)
+        
+        logging.info(f"===== Updating Target Chassis =====")
+        try:
+            upgrade_target_chassis(ip=ip, username=username, password=password, uploadId=uploadId, list_target_chassis_only_dict=list_target_chassis_only_dict)
+        except Exception as e:
+           if "timeout" not in str(e):
+               raise     
+    elif confirmation.lower() == "n":
+        upgrade_commander()
+        
+        
+if __name__ == "__main__":
+    upgrade_commander()
 
 
-# Make consolidated, list of IDs, username, password
-list_target_chassis_only_dict = get_target_chassis_information(ip="10.36.236.121", username="admin", password="somepassword", 
-                                                               target_chassis_list=TARGET_CHASSIS_DICT)
-print(list_target_chassis_only_dict)
+"""Output:
 
-print(list_target_chassis_only_dict)
-
-# Authenticate all the added chassis
-authenticate_target_chassis(ip="10.36.236.121", username="admin", password="somepassword", list_target_chassis_only_dict=list_target_chassis_only_dict)
-
-import time; time.sleep(30)
-# Upgrade all the added chassis
-try:
-    upgrade_target_chassis(ip="10.36.236.121", username="admin", password="somepassword", uploadId=2, list_target_chassis_only_dict=list_target_chassis_only_dict)
-except Exception as e:
-   if "timeout" not in str(e):
-       raise 
+(base) ashwjosh@C0HD4NKHCX ixupgradeutility % /usr/local/bin/python3 /Users/ashwjosh/ixupgradeutility/controller.py
+INFO:root:Controller Chassis is 10.36.236.121 with username : admin and password : Kimchi123Kimchi123!
+INFO:root:===== Fetching exisiting upgrade packages present on Controller Chassis 10.36.236.121
+ID - VERSION
+1 - 9.20.2700.12
+2 - 9.30.3001.12
+Please enter ID of version you want to upgeade to: 
+2
+You have selected to update target chassis to ID: 2. Please enter [Y/y] to proceed or [N/n] to re-enter: y
+INFO:root:===== Adding Target Chassis from config file on Controller Chassis 10.36.236.121
+Polling for async operation ...
+Completed async operation
+INFO:root:===== Target Chassis added on Controller Chassis 10.36.236.121. Please check at https://10.36.236.121/chassislab/chassisui/settings/ixos-install
+INFO:root:===== Consolidating chassis credentials and target chassis id's added in previous step
+INFO:root:[{'id': 22, 'ip': '10.36.237.131', 'activeVersion': 'NA', 'username': 'admin', 'password': 'Ixiaixiaixia!23'}]
+INFO:root:===== Authenticating all target chassis added on Controller Chassis 10.36.236.121
+Polling for async operation ...
+Completed async operation
+INFO:root:===== Authentication complete for target chassis. Please check at https://10.36.236.121/chassislab/chassisui/settings/ixos-install
+INFO:root:===== Sleeping for 30 secs to ensure all target chassis complete authentication =====
+INFO:root:===== Updating Target Chassis =====
+INFO:root:Upgrade started on 10.36.237.131
+Polling for async operation ...
+Completed async operation
+22
+Polling for async operation ...
+Completed async operation
+"""
